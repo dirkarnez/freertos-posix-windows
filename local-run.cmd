9bdb023cb1267ed3f8616f57213845b44d1e430f
Dirk Arnez <smalldirkalex@gmail.com>
2025-11-08 23:38:11 +0800

    - update local-run.cmd

--- local-run.cmd
+++ local-run.cmd
@@ -1,3 +1,4 @@
-start "" /b .\cmake-build\posix_demo.exe &&^
+start "" /b .\cmake-build\posix_demo.exe
+
 C:\Windows\System32\timeout.exe /t 20 &&^
 C:\Windows\System32\taskkill /f /im posix_demo.exe
